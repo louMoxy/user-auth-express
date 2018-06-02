6ccd225c996c2648acec7737c815e75c787a41d8
unknown <louisemoxy@gmail.com>
2018-06-02 22:05:07 +0100

    Add middleware to store sessions in db to save RAM

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var app = express();
 
 // Default port 27017
@@ -14,7 +15,10 @@ db.on('error', console.error.bind(console, 'connection error: '));
 app.use(session({
   secret: 'Moxy is the best',
   resave: true,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
 }))
 
 // make the user ID available in the template
